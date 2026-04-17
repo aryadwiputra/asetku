14f893345e5ab5e2d98ad033e24b46e2e4165667
Arya Dwi Putra <aryadptr.developer@gmail.com>
2026-04-17 08:44:27 +0700

    chore(wayfinder): add lifecycle actions

---
+++ resources/js/actions/App/Http/Controllers/AssetLifecycleConditionController.ts
@@ -0,0 +1,58 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
+
+/**
+* @see \App\Http\Controllers\AssetLifecycleConditionController::store
+* @see app/Http/Controllers/AssetLifecycleConditionController.php
+* @route '/asset-lifecycle/{asset}/condition'
+*/
+export const store = (args: { asset: number | { id: number } } | [asset: number | { id: number }] | number | { id: number }, options?: RouteQueryOptions): RouteDefinition<'post'> => ({
+    url: store.url(args, options),
+    method: 'post',
+})
+
+store.definition = {
+    methods: ["post"],
+    url: '/asset-lifecycle/{asset}/condition',
+} satisfies RouteDefinition<["post"]>
+
+store.url = (args: { asset: number | { id: number } } | [asset: number | { id: number }] | number | { id: number }, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { asset: args }
+    }
+
+    if (typeof args === 'object' && !Array.isArray(args) && 'id' in args) {
+        args = { asset: args.id }
+    }
+
+    if (Array.isArray(args)) {
+        args = { asset: args[0] }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const assetId = typeof args.asset === 'object' ? args.asset.id : args.asset
+
+    return store.definition.url
+        .replace('{asset}', assetId.toString())
+        .replace(/\/+$/, '') + queryParams(options)
+}
+
+store.post = (args: { asset: number | { id: number } } | [asset: number | { id: number }] | number | { id: number }, options?: RouteQueryOptions): RouteDefinition<'post'> => ({
+    url: store.url(args, options),
+    method: 'post',
+})
+
+const storeForm = (args: { asset: number | { id: number } } | [asset: number | { id: number }] | number | { id: number }, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: store.url(args, options),
+    method: 'post',
+})
+storeForm.post = (args: { asset: number | { id: number } } | [asset: number | { id: number }] | number | { id: number }, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: store.url(args, options),
+    method: 'post',
+})
+store.form = storeForm
+
+const AssetLifecycleConditionController = { store }
+
+export default AssetLifecycleConditionController
+
